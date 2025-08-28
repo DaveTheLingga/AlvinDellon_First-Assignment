4152825bd3379ba31243a42e3fcd46e054fb31e5
Alvin Dellon <alvindellon01@gmail.com>
2025-08-28 16:36:08 +0700

    Update AlvinDellon_First-Assignment.py

--- AlvinDellon_First-Assignment.py
+++ AlvinDellon_First-Assignment.py
@@ -1,2 +1,5 @@
-print("Name : Alvin Dellon")
+print("Name                          : Alvin Dellon")
 print("Favorite Programming Language : Python")
+i = 5
+for i in range (i):
+    print(" "*(5-i) + "#"*i + "#"*i)
